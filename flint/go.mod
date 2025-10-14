d8c69dabf8d7e0f9e9aa0256d3d3f2679bf0e551
SuperTapood <59110744+SuperTapood@users.noreply.github.com>
2025-10-14 14:28:24 +0300

    stuff

--- flint/go.mod
+++ flint/go.mod
@@ -1,4 +1,4 @@
-module flint
+module github.com/SuperTapood/Flint
 
 go 1.25.3
 
--- flint/main.go
+++ flint/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	gen "github.com/SuperTapood/Flint/generated"
+	"google.golang.org/protobuf/proto"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	data, err := os.ReadFile("../bob.bin")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var person gen.Person
+
+	if err := proto.Unmarshal(data, &person); err != nil {
+		log.Fatal("failed to unmarshal:", err)
+	}
+
+	log.Printf("Loaded user: %+v\n", person)
 }
